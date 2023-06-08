6ea91468b30d3f107987ce5b4d136f63d7d5a2bd
unknown <sandra.gutierrez.ru@gmail.com>
2023-06-08 12:07:47 -0500

    updated plugins, added IndexedDB, asset caching

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,66 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file. ///
 // TODO: Add CSS loaders and babel to webpack. ///
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'J.A.T.E',
-        favicon: './favicon.ico'
+        template: "./index.html",
+        title: "Webpack Plugin",
+        favicon: "./favicon.ico",
       }),
       new WebpackPwaManifest({
-        filename: 'manifest.json',
-        name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'text editor PWA',
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "text editor PWA",
+        inject: true,
+        fingerprints: false,
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve(__dirname, "./src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',})
+        swSrc: path.resolve(__dirname, "./src-sw.js"),
+        swDest: "src-sw.js",
+      }),
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
